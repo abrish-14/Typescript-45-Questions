fb61c92caa4c4b2b1226453b389f91551d21114b
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-02-22 21:45:00 +0500

    Delete QUESTION19.ts

--- QUESTION19.ts
+++
@@ -1,8 +0,0 @@
-//Question 19 Guest list:
-
-let _guestList = ["madiha", "arooba", "laiba"];
-for (let guest of _guestList) {
-    console.log(`Dear ${guest}, you are invited to dinner.`);
-}
-console.log(`Total number of people invited to dinner: ${_guestList.length}`);
-
