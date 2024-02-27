4ae4a5a904591e1f360bc53010f5509e0c11148e
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-02-27 20:58:16 +0500

    Delete QUESTION39.ts

--- QUESTION39.ts
+++
@@ -1,9 +0,0 @@
-//Question 39 city Names:
-
-function city_country(city: string, country: string): string {
-    return `${city}, ${country}`;
-  }
-  console.log(city_country('Lahore', 'Pakistan'));
-  console.log(city_country('Tokyo', 'Japan'));
-  console.log(city_country('Paris', 'France'));
-  
