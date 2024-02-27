a85af14569eabcbd5c2a6bf1c20d4157275f6af3
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-02-27 20:57:04 +0500

    Delete QUESTION34.ts

--- QUESTION34.ts
+++
@@ -1,8 +0,0 @@
-//Question 34 pizzas
-let favoritePizzas = ['beaf', 'margherita', 'supreme'];
-
-for (let pizza of favoritePizzas) {
-  console.log(`I like ${pizza} pizza.`);
-}
-
-console.log("Pizza is one of my favorite foods, I really love it!");
