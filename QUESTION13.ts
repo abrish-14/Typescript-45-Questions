6de990037f26d2da1fb0e1fe4d9c39ecdb31de8d
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-02-27 20:53:34 +0500

    Delete QUESTION13.ts

--- QUESTION13.ts
+++
@@ -1,7 +0,0 @@
-
-//Question 13 Your Own Array: 
-
-  let vehicles= ["Car","Bicycle","Motorcycle","Scooter"];
-  for (let i =0; i < vehicles.length; i++)
-  {console.log ("i would love to ride a "+ vehicles[i] + "someday!");
-  }
