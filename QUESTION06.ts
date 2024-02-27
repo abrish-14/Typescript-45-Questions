dedba59f97108d41d02e6572aa535f6920d7e2a8
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-02-27 20:51:59 +0500

    Delete QUESTION06.ts

--- QUESTION06.ts
+++
@@ -1,6 +0,0 @@
-//Question 6  : stripping names :
-
-let identity="\t \n kamran rathoor \t\n";
-console.log("identity with whitespace:" + identity);
-identity=identity.trim();
-console.log("idetity without whitespace:" + identity);
