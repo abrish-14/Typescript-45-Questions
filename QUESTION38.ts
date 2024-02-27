249adf3987a15f32d1678dd8b33bddd94533c715
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-02-27 20:57:55 +0500

    Delete QUESTION38.ts

--- QUESTION38.ts
+++
@@ -1,9 +0,0 @@
-//Question 38 Cities :
-function describe_city(city: string, country: string = 'default country'): void {
-    console.log(`${city} is in ${country}.`);
-  }
-  
-  describe_city('Karachi', 'Pakistan');
-  describe_city('New York', 'USA');
-  describe_city('London');
-  
