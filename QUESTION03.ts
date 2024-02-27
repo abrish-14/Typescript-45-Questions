a6b573a7cc8f69607882ccba7c8098e2a0bebc3b
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-02-27 20:51:07 +0500

    Delete QUESTION03.ts

--- QUESTION03.ts
+++
@@ -1,6 +0,0 @@
-//Question 3 Name case:
-
-let Name ="abrish";
-console.log(Name.toLowerCase());
-console.log(Name.toUpperCase());
-console.log(Name.charAt(0).toUpperCase()+Name.slice(1).toLowerCase());
