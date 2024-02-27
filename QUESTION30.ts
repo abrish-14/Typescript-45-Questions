8e3646b6363b96fc795e1b09758f5ebc7a90861f
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-02-27 20:56:07 +0500

    Delete QUESTION30.ts

--- QUESTION30.ts
+++
@@ -1,9 +0,0 @@
-// Question 30 Hello admin:
-let user_names = ['maliha', 'madiha', 'maria', 'marahila', 'maham'];
-for (let username of user_names) {
-  if (username === 'admin') {
-      console.log("Hello admin, would you like to see a status report?");
-  } else {
-      console.log(`Hello ${username}, thank you for logging in again.`);
-  }
-}
