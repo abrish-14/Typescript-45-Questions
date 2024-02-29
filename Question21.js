a550a5d787088cfaa64b921e5727d583fa44ca6f
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-01 00:42:23 +0500

    Add files via upload

---
+++ Question21.js
@@ -0,0 +1,8 @@
+/*They think of something you could store in a TypeScript Object. Write a program
+ that creates Objects containing these items. in typescript*/
+var myFruit = {
+    name: "Apple",
+    color: "Red",
+    isTasty: true
+};
+console.log("My Fruit:", myFruit);
