3431e9546748d0a37d2d4b7a09c3ffd5527f2716
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-03 17:02:03 +0500

    Delete Question04.js

--- Question04.js
+++
@@ -1,6 +0,0 @@
-/*Question 4 Famous Quote: Find a quote from a famous person you admire. Print the quote and
-the name of its author. Your output should look something like the following,
- including the quotation marks:*/
-var quote = "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.";
-var author = "Albert Einstein";
-console.log("\"".concat(quote, "\" - ").concat(author));
