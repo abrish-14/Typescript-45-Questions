9e33d21fbba7bc0ef54f46eb641c0625079665a4
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-03 17:04:16 +0500

    Delete Question10.ts

--- Question10.ts
+++
@@ -1,9 +0,0 @@
-/*Question 10 Adding Comments: Choose two of the programs you’ve written, and
-add at least one comment to each. If you don’t have anything
-specific to write because your programs are too simple at this point,
-just add your name and the current date at the top of each program file.
- Then write one sentencedescribing what the program does.*/
-
-// its is my first assignment 
- //my name is abrishrathoor
-
