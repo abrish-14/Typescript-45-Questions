8cb437a1a08901e33dad4de1912ba568ef1a8497
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-03 17:08:17 +0500

    Delete Question21.js

--- Question21.js
+++
@@ -1,8 +0,0 @@
-/*They think of something you could store in a TypeScript Object. Write a program
- that creates Objects containing these items. in typescript*/
-var myFruit = {
-    name: "Apple",
-    color: "Red",
-    isTasty: true
-};
-console.log("My Fruit:", myFruit);
