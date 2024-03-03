d203cc1eacdfcd2ad185894aeca11f76999508e0
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-03 18:25:26 +0500

    Add files via upload

---
+++ 04 Famouse Quote/Question04.ts
@@ -0,0 +1,10 @@
+/*Question 4 Famous Quote: Find a quote from a famous person you admire. Print the quote and 
+the name of its author. Your output should look something like the following,
+ including the quotation marks:*/
+
+ const quote: string = "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.";
+const author: string = "Albert Einstein";
+
+console.log(`"${quote}" - ${author}`);
+
+
