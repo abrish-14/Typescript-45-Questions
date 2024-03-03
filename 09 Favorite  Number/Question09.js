f204352a302e267ad04707f361c48187e1bf9ecc
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-03 19:19:03 +0500

    Delete 09 Favorite  Number directory

--- 09 Favorite  Number/Question09.js
+++
@@ -1,6 +0,0 @@
-/*Question 9 Favorite Number: Store your favorite number in a variable. Then,
- using that variable, create a message that reveals your favorite number.
- Print that message.*/
-var favoriteNumber = 14;
-var Message = "My favorite number is ".concat(favoriteNumber, ".");
-console.log(Message);
