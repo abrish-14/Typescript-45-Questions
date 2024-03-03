7385bd1157179d40f8d5a8a7d1f6223a5d26fa54
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-03-03 19:18:10 +0500

    Delete 05 Famouse Quote 2 directory

--- 05 Famouse Quote 2/Question05.js
+++
@@ -1,7 +0,0 @@
-/*Question 4Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s
- name in a variable called famous_person. Then compose your message and
-  store it in a new variable called message. Print your message.*/
-var famousPerson = "Albert Einstein";
-var quote = "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.";
-var message = "\"".concat(quote, "\" - ").concat(famousPerson);
-console.log(message);
